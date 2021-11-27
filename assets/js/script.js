ebaad274815049f218afb5180a398b85ed0b98c5
Cadenza <mirandalucsb@gmail.com>
2021-11-26 18:19:52 -0700

    added disabled state for ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var ingredients = ["honeycap","inkycap", "orangecrown" , "rondobello", "scarletflytrap"];
-
 var currentIngredients = [];
+var ingredientCounter = 0;
+var bodyEl = $("body");
 
 var getIngredientName = function(src){
     for(var i = 0; i<ingredients.length;i++){
@@ -14,18 +15,34 @@ $(".ing-div").draggable({
     helper: "clone",
     appendTo: "body",
     start : function(event,ui){
+        if(ingredientCounter > 2){
+            $(".ing-div").draggable({disabled: true});
+         }
         //event.target.style.backgroundColor = null;
+        
     },
 
 });
 
+var disableIngredients = function(){
+    $(".ing-card").attr("style","background-color:lightgrey;");
+    $(".listen-btn").attr("style", "background-color:grey; border: 1px solid black; opacity: 50%");
+    $(".ing-div").attr("style","opacity: 50%");
+}
+
 $(".cauldron-drop").droppable({
     drop: function(event, ui) {
+        if(ingredientCounter > 2){
+           disableIngredients();
+        }
+        else if(ingredientCounter > 3){
+            return;
+        }
         //ui.draggable.remove();
         var src = ui.draggable[0].innerHTML.replace("<img class=" +'"' +"ingredient w-50" +'"'+ " src=", "")
         src = src.replaceAll('"','')
         src = src.replace('>','');
-        console.log(src);
+        //console.log(src);
         event.target.style.backgroundColor = null;
         //update current ingredients array
         var divEl = $(".current-ing");
@@ -38,6 +55,7 @@ $(".cauldron-drop").droppable({
         cardHeader.attr("class","card-header small");
         cardHeader.text(getIngredientName(src));
         currentIngredients.push(getIngredientName(src));
+        ingredientCounter++;
         var imageDiv = $("<div>");
         //imgEl.attr("src","assets/images/honeycap.png");
         imgEl.attr("src", src);
@@ -51,4 +69,13 @@ $(".cauldron-drop").droppable({
       over: function(event, ui) {
           event.target.style.backgroundColor = "rgba(242, 240, 184,0.2)";
       },
+});
+ bodyEl.on("mouseover",".ing-card",function(event){
+        if(ingredientCounter < 4){
+            $(event.target).closest(".ing-card").addClass("hover");
+        } 
+});
+
+bodyEl.on("mouseout",".ing-card",function(event){
+    $(event.target).closest(".ing-card").removeClass("hover")
 });
